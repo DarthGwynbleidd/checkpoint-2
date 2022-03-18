bb7a4b61907b7bb051d386da92b85db786e99d7b
unknown <gaeldouence@gmail.com>
2022-03-18 10:49:35 +0100

    filter games above 4.5 rating with button

--- react-games/src/components/GameList.js
+++ react-games/src/components/GameList.js
@@ -5,19 +5,28 @@ import Game from './Game';
 const GameList = () => {
 
     const [gameList, setGameList] = useState([])
+    const [check, setCheck] = useState(false)
 
     useEffect(() => {
         async function fetchGameList (){
             const response = await axios.get('https://apis.wilders.dev/wild-games/games')
             setGameList(response.data)
+            console.log(gameList);
         }
 
         fetchGameList()
     }, [])
 
+    const handleClick = () => {
+        setCheck(prevCheck => prevCheck = !prevCheck)
+    }
+
     return (
         <div>
-            {gameList.map((element, index) => {
+            <label>Show games rating above 4.5</label>
+            <button onClick={handleClick}>{check ? 'ON' : 'OFF'}</button>
+            {gameList.filter((game) => !check || game.rating >= 4.5)
+            .map((element, index) => {
                 return(
                     <Game key={index} game={element}/>
                 )
