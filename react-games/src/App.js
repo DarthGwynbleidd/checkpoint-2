e328161c04fed9e25fec575f69cf8667937ea819
unknown <gaeldouence@gmail.com>
2022-03-18 09:47:42 +0100

    init react app & Header component

---
+++ react-games/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header name={"React-games"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-games/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <div>
+            Welcome in {props.name}
+        </div>
+    );
+};
+
+export default Header;
